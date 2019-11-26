41deee02e8a37598ade5b94cc1e19bea39683775
danny <danny.eeraerts@proximus.be>
2019-11-26 14:01:56 +0100

    Add information function about director

--- film.js
+++ film.js
@@ -1,5 +1,4 @@
 const items = document.querySelectorAll(".nav-item");
-console.log(items);
 //input-variabel-search-one
 const inputTitle = document.querySelector(".input-title");
 var inputTitleValue = inputTitle.value;
@@ -26,11 +25,7 @@ let year = new Date().getFullYear();
 const errorText = document.querySelector(".error");
 const errorText2 = document.querySelector(".error2");
 
-
-
-
 htmlYear.textContent = year;
-//console.log(target);
 
 function toggleActiveClass(){
 	let current = document.querySelector(".active");
@@ -54,7 +49,6 @@ function searchFilm(inputTitleValue){
 	
 	//clear result
 	clearTarget();
-	console.log(inputTitleValue);
 	axios.get(firstPartHttpLink + "t=" + inputTitleValue + lastPartHttpLink)
 	.then(response => {	
 		let result = response.data;
@@ -77,12 +71,9 @@ function searchFilm(inputTitleValue){
 function searchFilm2(inputTitlePartValue){
 
 		clearTarget2();
-		//var inputTitlePartValue = inputTitlePart.value;
-		console.log(inputTitlePartValue);
 		axios.get(firstPartHttpLink + "s=" + inputTitlePartValue + lastPartHttpLink)
 		.then(response => {
 			let result2 = response.data;
-			console.log(result2);
 			let length = result2.Search.length;
 			buildtemplate2(result2, length);
 	
@@ -103,8 +94,8 @@ function searchFilm2(inputTitlePartValue){
 		}
 		tmpl.querySelector('.title').innerText = result.Title;
 		let directorsString = result.Director;
-		let directors = directorsStringToActorsArrayAndBack(directorsString);
-		tmpl.querySelector('.movieDirector').innerHTML = "<span class='font-weight-bold'>Director:</span>" + " " + directors;
+		let directors = directorsStringToDirectorsArrayAndBack(directorsString);
+		tmpl.querySelector('.movieDirector').innerHTML = "<span class='font-weight-bold' data-toggle='modal' data-target='#exampleModal'>Director:</span>" + " " + directors;
 		let actorsString = result.Actors;
 		let actors = actorsStringToActorsArrayAndBack(actorsString);
 		tmpl.querySelector('.movieActors').innerHTML = "<span class='font-weight-bold' data-toggle='modal' data-target='#exampleModal'>Actors:</span>" + " " + actors;
@@ -118,12 +109,12 @@ function searchFilm2(inputTitlePartValue){
 
 	}
 
-	function directorsStringToActorsArrayAndBack(directorsString){
+	function directorsStringToDirectorsArrayAndBack(directorsString){
 		var arrayOfDirectors = directorsString.split(", ");
 		let numberOfDirectors = arrayOfDirectors.length;
 		let directorsNewString= "";
 		for (let i=0; i<numberOfDirectors;i++){
-			directorsNewString += `<span class= "director${i}">${arrayOfDirectors[i]}</span>`
+			directorsNewString += `<span class= "director${i} director">${arrayOfDirectors[i]}</span>`
 			if(i< (numberOfDirectors -1 )){
 				directorsNewString += ", "}
 		}
@@ -132,11 +123,10 @@ function searchFilm2(inputTitlePartValue){
 
 	function actorsStringToActorsArrayAndBack(actorsString){
 		arrayOfActors = actorsString.split(", ");
-		console.log(arrayOfActors[0]);
 		let numberOfActors = arrayOfActors.length;
 		let actorsNewString= "";
 		for (let i=0; i<numberOfActors;i++){
-			actorsNewString += `<span class= "actor${i}">${arrayOfActors[i]}</span>`
+			actorsNewString += `<span class= "actor${i} actor">${arrayOfActors[i]}</span>`
 			if(i< (numberOfActors -1 )){ 
 				actorsNewString += ", "}
 		}
@@ -146,8 +136,6 @@ function searchFilm2(inputTitlePartValue){
 	function buildtemplate2(result2, length){
 		for(i=0; i<length ;i++){
 			var tmpl2 = template2.content.cloneNode(true);
-			console.log("template2");
-			//console.log(tmpl2);
 			if (result2.Search[i].Poster == "N/A"){
 				tmpl2.querySelector('.moviePoster').src = "Image-not-available.jpg";
 			} 
@@ -155,40 +143,61 @@ function searchFilm2(inputTitlePartValue){
 				tmpl2.querySelector('.moviePoster').src = result2.Search[i].Poster;
 			}
 			tmpl2.querySelector('.moviePoster').setAttribute("class", "poster"); 
-			//console.log(tmpl2.querySelector('.moviePoster').src);
 			tmpl2.querySelector('.title').innerText = result2.Search[i].Title;
-			//console.log(result2.Search[i].Title);
-			//console.log(tmpl2.querySelector('.title').innerText);
 			target2.appendChild(tmpl2);
 		}
 	}
 
 	function makeModal(){
 		// Get modal elements
-		var modal = document.querySelector('.my-modal');
-		var modalBtn = document.querySelector('.actor0');
+		modal = document.querySelector('.my-modal');
+		//var modalBtn = document.querySelector('.actor0');
 		var closeBtn = document.querySelector('.close');
+		var modalBtn = document.querySelector('.actorList');
+		var modalBtn2 = document.querySelector('.directorList');
+
 		// Events
 		modalBtn.addEventListener('click', openModal);
+		modalBtn2.addEventListener('click',openModal2);
 		closeBtn.addEventListener('click', closeModal);
 		window.addEventListener('click', outsideClick);
 		// Open
-		function openModal() {
+		function openModal(event) {
 			modal.style.display = 'block';
 			const actorNameInModal = document.querySelector(".actorNameModal");
-			let actorString = arrayOfActors[0];
+			var choosenActor = event.target.closest(".actor").textContent;
+			console.log("this");
+			console.log(choosenActor);
+			//let actorString = arrayOfActors[0];
+			let actorString = choosenActor;
 			let otherMovies = searchOtherMoviesOfThisActor(actorString);
-			actorNameInModal.textContent = arrayOfActors[0];
+			console.log(otherMovies);
+			//actorNameInModal.textContent = arrayOfActors[0];
+			actorNameInModal.textContent = actorString;
 		}
 		// Close
-		function closeModal() {
+		function closeModal(){
 			modal.style.display = 'none';
-		}
+		};
+		
 		// Close If Outside Click
 		function outsideClick(e) {
 			if (e.target == modal) {
 				modal.style.display = 'none';
 			}
+		}
+		function openModal2(event) {
+			modal.style.display = 'block';
+			const directorNameInModal = document.querySelector(".directorNameModal");
+			var choosenDirector = event.target.closest(".director").textContent;
+			console.log("this");
+			console.log(choosenDirector);
+			//let actorString = arrayOfActors[0];
+			let directorString = choosenDirector;
+			let otherMovies = searchOtherMoviesOfThisActor(directorString);
+			console.log(otherMovies);
+			//actorNameInModal.textContent = arrayOfActors[0];
+			directorNameInModal.textContent = directorString;
 		}
 	}
 
@@ -203,19 +212,16 @@ function searchFilm2(inputTitlePartValue){
 				newActorListString += actorNamesArray[i];
 			}
 		}
-		console.log(newActorListString);
-		console.log("actor name hieboven");
 		axios.get("http://api.tmdb.org/3/search/person?api_key=123e8de41103fbaabc178ff9d89ea8dd&query="+newActorListString)
 		.then(response => {
 			let extraMovies = response.data;
+			//console.log(extraMovies);
+			let id = extraMovies.results[0].id;
 			let length = (extraMovies.results[0].known_for.length);
-			let list = document.getElementsByClassName("alternativeMovie");
-			let parent = list.parent;
-		  if (list.lenght != 0){
-				for (let i=1; i=list.length; i++){
-					parent[i].removeChild(parent.childNodes[i]);
-				}
-				console.log(list[0]);
+			let listCountlength = document.getElementsByClassName("alternativeMovie").length;
+		  if (listCountlength> 0){
+				let parent = document.getElementById("target3");
+					parent.innerHTML = "";
 			} 
 			for (let i=0; i<length; i++){
 				const template3 = document.getElementById("otherMovieTemplate");
@@ -223,12 +229,43 @@ function searchFilm2(inputTitlePartValue){
 				tmpl3.querySelector('li').setAttribute("class", "alternativeMovie");
 				tmpl3.querySelector('li').innerText = extraMovies.results[0].known_for[i].title;
 				target3.appendChild(tmpl3);
-				
 			}
+			biographyInfo(id);
+			target3.addEventListener("click",newSearch);
 		})
 		.catch(error => {
 			console.log("This film is not found , please try again");
 		});
+
+	}
+
+	function newSearch(){
+		modal.style.display = 'none';
+		var movie = event.target.closest(".alternativeMovie");
+		inputTitleValue = movie.innerText;
+		searchFilm(inputTitleValue);
+	}
+
+	function biographyInfo(id){
+		axios.get(`https://api.themoviedb.org/3/person/${id}?api_key=123e8de41103fbaabc178ff9d89ea8dd`)
+			.then(response => {
+				let extraInfo = response.data;
+				const bioInfo = document.querySelector(".bio");
+				const birth = document.querySelector(".birth");
+				const death = document.querySelector(".death");
+				const place = document.querySelector(".place");
+				const imageActor= document.querySelector(".imgActor");
+				let imageSource= extraInfo.profile_path;
+				imageActor.src =`https://image.tmdb.org/t/p/original/${imageSource}`;
+				bioInfo.textContent = extraInfo.biography;
+				birth.textContent = extraInfo.birthday;
+				death.textContent = extraInfo.deathday;
+				place.textContent = extraInfo.place_of_birth;
+				
+			})
+			.catch(error => {
+				console.log("This actor or director is not found , please try again");
+			});
 	}
 
 	function refresh(){
@@ -244,26 +281,19 @@ function searchFilm2(inputTitlePartValue){
 	function searchClickedMovie(event){
 		var poster = event.target.closest(".poster");
 		inputTitleValue = poster.parentNode.nextElementSibling.firstElementChild.innerText;
+		scroll(0,0);
 		searchFilm(inputTitleValue);
 	}	
 	
 	function clearTarget(){
-		//console.log("target wordt leeggemaakt");
-		//console.log(document.querySelector('#target'));
 		document.querySelector('#target').innerHTML = "";
 	}
 
 	function clearTarget2(){
-		//console.log("target wordt leeggemaakt");
-		//console.log(document.querySelector('#target'));
 		document.querySelector('#target2').innerHTML = "";
 	}
 
-	function checkInput(){
-		//console.log("test");
-		console.log(inputTitle.value);
-		//console.log(inputTitle.value.length);
-		
+	function checkInput(){		
 		if (inputTitle.value.length <= 0) {
 			clearTarget();
 		}
@@ -273,10 +303,6 @@ function searchFilm2(inputTitlePartValue){
 	}
 
 	function checkInput2(){
-		//console.log("test");
-		console.log(inputTitlePart.value);
-		//console.log(inputTitle.value.length);
-		
 		if (inputTitlePart.value.length <= 0) {
 			clearTarget();
 		}
@@ -309,30 +335,4 @@ function searchFilm2(inputTitlePartValue){
 
 
 
-
-
-
-
-	test();
-		
-	function test(){
-
-		/* axios.get("https://api.themoviedb.org/3/person?api_key=123e8de41103fbaabc178ff9d89ea8dd&query=tom%20hanks")*/
-
-		/*http://api.tmdb.org/3/search/person?api_key=KEY&query=tom%20hanks*/
-
-		axios.get("http://api.tmdb.org/3/search/person?api_key=123e8de41103fbaabc178ff9d89ea8dd&query=steven%20spielberg")
-		
-
-		.then(response => {
-			let testresult = response.data;
-			console.log(testresult);
-			let length = (testresult.results[0].known_for.length);
-			for (let i=0; i<length; i++){
-			console.log(testresult.results[0].known_for[i].title);}
 	
-			})
-		.catch(error => {
-			console.log("This actor is not found , please try again");
-		});
-	}
